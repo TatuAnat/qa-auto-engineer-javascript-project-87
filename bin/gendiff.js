5038e37606b79472724a5bdbe501be1a4a04a924
TatuAnat <156831202+TatuAnat@users.noreply.github.com>
2025-12-15 18:41:31 +0100

    create gendiff.js and index.js; add commander.js

---
+++ bin/gendiff.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander'
+import genDiff from '../src/index.js'
+
+const program = new Command()
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  
+  .arguments('<filepath1> <filepath2>')
+
+program.parse(process.argv)
+
+const options = program.opts()
+const args = program.args
+
+if (args.length < 2) {
+  console.error("error: missing required argument(s) 'filepath1' and 'filepath2'")
+  program.help({ error: true })
+} else {
+  const [filepath1, filepath2] = args
+  try {
+    console.log(genDiff(filepath1, filepath2, options.format))
+  } catch (err) {
+    console.error(err.message)
+    process.exit(1)
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const genDiff = (filepath1, filepath2, outputFormat = 'stylish') => {
+  return "gen DIFF"
+}
+
+export default genDiff
