6625cd75f0eff7435e1f6d3aaea3bfdd367e6e37
TatuAnat <156831202+TatuAnat@users.noreply.github.com>
2026-01-21 22:01:11 +0100

    fix gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander'
-import path from 'node:path'
 import genDiff from '../src/index.js'
 
 const program = new Command()
@@ -12,17 +11,8 @@ program
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const resolved1 = path.resolve(process.cwd(), filepath1)
-    const resolved2 = path.resolve(process.cwd(), filepath2)
-
-    try {
-      const result = genDiff(resolved1, resolved2, program.opts().format)
-      console.log(result)
-    }
-    catch (err) {
-      console.error(err.message)
-      process.exit(1)
-    }
+    const result = genDiff(filepath1, filepath2, program.opts().format)
+    console.log(result)
   })
 
 program.parse(process.argv)
